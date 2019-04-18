32df62c23b9df8dae7df3139662d5d41f7d8c869
Gerardo Manzo <programmingmanzo@gmail.com>
2019-04-18 10:02:58 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -6,7 +6,7 @@ class Teacher < User
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
   
   def teach 
-    random_string = KNOWLEDGE.sample(1 + rand(KNOWLEDGE.count))
+    random_string = KNOWLEDGE.sample
     random_string
     #binding.pry
   end 
